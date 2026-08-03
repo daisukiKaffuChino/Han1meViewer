b0ed2280ac003d59285ace086578b19cc1c11572
zzzxxxxxxxxxx <2409974882@qq.com>
2026-08-03 18:09:45 +0800

    fix: import collect for mylist flow collection

--- app/src/main/java/io/github/daisukikaffuchino/han1meviewer/ui/viewmodel/MyPlayListViewModel.kt
+++ app/src/main/java/io/github/daisukikaffuchino/han1meviewer/ui/viewmodel/MyPlayListViewModel.kt
@@ -24,6 +24,7 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
